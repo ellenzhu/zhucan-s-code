d634a06243d6a4910afbf42ab8b9da98dd2ea500
Lassi Haaranen <haaranl1@niksula.hut.fi>
2014-10-28 12:54:03 +0200

    First two lectures and first three exercise rounds added

---
+++ exercises/03_javascript/linkdecoration.js
@@ -0,0 +1,21 @@
+// Exercise 2.3 Javascript file  //
+// Read the instructions from NG //
+
+//some helpful debug code
+
+$("#javascript_start").html("[OK] Started executing the javascript file");
+$("#javascript_end").html("[WAITING]...this far we haven't reached the end... Maybe you should try FireBug?");
+
+// ADD YOUR CODE BETWEEN THESE LINES //
+
+
+
+
+
+
+
+// ADD YOUR CODE BETWEEN THESE LINES //
+
+//some helpful debug code
+
+$("#javascript_end").html("[OK] The end of your javascript file was reached. (meaning there were no huge errors) Hopefully your code works too! ")
---
+++ lectures/README.md
@@ -0,0 +1,4 @@
+Lecture material
+
+intro_to_wsd.html - Introductory lecture
+html_css.html - Introduction to HTML and CSS (and HTTP)
